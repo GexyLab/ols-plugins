6ce46e37c4054414b0b58be2fe848ce6a2f50e88
Eugenio Liguori <e.liguori@gexy.it>
2026-03-12 19:39:33 +0100

    Rename project

--- Projects directory/ols-plugin-projects-directory.cs
+++ Projects directory/ols-plugin-projects-directory.cs
@@ -3,11 +3,14 @@ using OpenLabSDK.error;
 using OpenLabSDK.events;
 using OpenLabSDK.plugin;
 using OpenLabSDK.ui;
+using System.Windows;
 
 namespace OpenLabStudio.plugins
 {
     public class OLSPluginProjectsDirectory : Plugin
     {
+        #region Variables
+
         internal class Info : IPluginInfo
         {
             public string Name { get; } = "ols-plugin-projects-directory";
@@ -17,6 +20,10 @@ namespace OpenLabStudio.plugins
             public string url { get; } = "https://github.com/GexyLab/ols-plugins/tree/main";
             public string description { get; } = "Load OpenLab Studio projects from directory";
         }
+
+        string projectsDir = "";
+
+        #endregion
 
         public OLSPluginProjectsDirectory(
             IErrorManager _errorManager,
@@ -30,21 +37,53 @@ namespace OpenLabStudio.plugins
 
         public override int deinit()
         {
-            log.info("Deinit plugin" + pluginInfo.Title);
+            log.info("Deinit plugin" + pluginInfo.Name);
             return 0;
         }
 
         public override int init()
         {
-            log.info("Init plugin " + pluginInfo.Title);
+            log.info($"Init plugin {pluginInfo.Title} ({pluginInfo.Name})");
 
-            eventsManager.addEventHandler("ols.ready", (object sender, EventArgs e) =>
+            
+
+            eventsManager.addEventHandler("projectManager.existingsProjectsLoad.before", (object sender, EventArgs e) =>
             {
-                log.info($"Plugin: {pluginInfo.Name} hello!!");
+                try
+                {
+                    initProjectsDir();
+
+                    MessageBox.Show(projectsDir);
+                }
+                catch { throw; }
+                
+
+                
                 return true;
             });
 
+            /*eventsManager.addEventHandler("ols.ready", (object sender, EventArgs e) =>
+            {
+                log.info($"Plugin: {pluginInfo.Name} hello!!");
+                return true;
+            });*/
+
             return 0;
+        }
+
+        private void initProjectsDir()
+        {
+            if (pluginDefinition.haveConfig())
+            {
+                projectsDir = (string)pluginDefinition.config.Get("dir", "none");
+            }
+            else
+            {
+                string msg = "Plugin not have it's specific config, missing config json object in OpenLab Studio config file";
+                log.error(msg);
+                throw new OLSException(errorManager, msg);
+            }
+                
         }
     }
 }
