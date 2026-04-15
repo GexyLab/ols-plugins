f4f94dcb9e92f9e4371d7207bcb912b5c55c9bd1
Eugenio Liguori <e.liguori@gexy.it>
2026-04-15 06:12:24 +0200

    Title changed

--- Projects directory/OLSPluginProjectsDirectory.cs
+++ Projects directory/OLSPluginProjectsDirectory.cs
@@ -19,11 +19,11 @@ namespace OpenLabStudio.plugins
         internal class Info : IPluginInfo
         {
             public string Name { get; } = "ols-plugin-projects-directory";
-            public string Title { get; } = "OpenLab Studio projects directory";
+            public string Title { get; } = "OpenLab Studio projects sorage directory";
             public int[] version { get; } = { 1, 0, 0 };
             public string vendorUID { get; }
             public string url { get; } = "https://github.com/GexyLab/ols-plugins/tree/main";
-            public string description { get; } = "Load OpenLab Studio projects from directory";
+            public string description { get; } = "Load OpenLab Studio projects from a local directory";
         }
 
         string? projectsDirPath;
@@ -45,21 +45,13 @@ namespace OpenLabStudio.plugins
 
         public override int deinit()
         {
-            log.info("Deinit plugin" + pluginInfo.Name);
+            log.info($"Deinit project storage {pluginInfo.Title} ({pluginInfo.Name})");
             return 0;
         }
 
         public override int init()
         {
             log.info($"Init project storage {pluginInfo.Title} ({pluginInfo.Name})");
-
-
-            /*eventsManager.addEventHandler("ols.ready", (object sender, EventArgs e) =>
-            {
-                log.info($"Plugin: {pluginInfo.Name} hello!!");
-                return true;
-            });*/
-
             return 0;
         }
 
